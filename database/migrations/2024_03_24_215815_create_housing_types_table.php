5057dc58b709da164f74a2e9634cc1fffe16c1d5
Zakiyou <zakiyoubabaodi@gmail.com>
2024-03-25 12:30:13 +0100

    creation de quelques tables

---
+++ database/migrations/2024_03_24_215815_create_housing_types_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('housing_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('is_deleted')->default(false);
+            $table->boolean('is_blocked')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('housing_types');
+    }
+};

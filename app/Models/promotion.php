4c94f1f212aa5a671c1403fdc9f0991ec1e8b5ce
ReloDev <ayenaaurel15@gmail.com>
2024-05-22 12:51:49 +0100

    modif

--- app/Models/promotion.php
+++ app/Models/promotion.php
@@ -5,21 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class promotion extends Model
+class Promotion extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'housing_id', 
-        'number_of_reservation',
         'value',
-        'is_encours', 
-        'is_deleted', 
-        'date_debut',
-        'date_fin',
+        'number_of_reservation'
     ];
-
-    public function housing()
-    {
-        return $this->belongsTo(Housing::class, 'housing_id'); 
-    }
 }
